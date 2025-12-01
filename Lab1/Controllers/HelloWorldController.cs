abc90cd06a8a249bed52aba5e91089128db088fa
BasementLoli <nhattung2801@gmail.com>
2025-12-01 11:03:52 +0700

    init 12/1/2025

---
+++ Lab1/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Lab1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "this is my default action...";
+        }
+
+        public string Welcome(string name, int numTimes = 4)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
